58ea33b73d66096e0929370532308d56f1d80f43
lcarvalhoufc <lcarvalho.developer@gmail.com>
2017-05-15 10:21:06 -0300

    initial code

---
+++ src/projeto2/ConexaoMySQL.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto2;
+
+/**
+ *
+ * @author leandro
+ */
+
+//Classes necessárias para uso de Banco de dados //
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class ConexaoMySQL {
+
+    private static ConexaoMySQL instance = null;
+    private Connection connection = null;
+    
+
+    private ConexaoMySQL() {
+        try {
+            
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+            
+            String serverName = "127.0.0.1";    
+            
+            //nesta linha estar� o nome do seu banco de dados. Olhe no workbench
+            //o nome do banco. Caso voce tenha criado o modelo no workbench e n�o
+            //tenha modificado o nome do banco, o nome padr�o � "mydb". 
+            String dbName = "projeto2_database" ;        
+            String url = "jdbc:mysql://" + 
+                    serverName + "/" + 
+                    dbName;
+            
+            //ajuste o username e password de acordo com o que voce utiliza para 
+            //acessar o banco
+            String username = "root";             
+            String password = "mars2809";      
+            
+            connection = DriverManager.getConnection(url,
+                    username, password);
+              
+            if (connection != null) {
+                System.out.println("STATUS--->Conectado "
+                        + "com sucesso!");
+            } else {
+                System.err.println("STATUS--->Não foi "
+                        + "possivel realizar conexão");
+            }
+            connection.setAutoCommit(true);
+        } catch (ClassNotFoundException e) {  
+            
+            System.out.println("O driver expecificado"
+                    + " nao foi encontrado.");
+        } catch (SQLException e) {
+            
+            System.out.println("Nao foi possivel"
+                    + " conectar ao Banco de Dados.");
+            e.printStackTrace();
+        }
+        }
+
+    public static ConexaoMySQL getInstance() {
+        if (instance == null) {
+            instance = new ConexaoMySQL();
+        }
+        return instance;
+    }
+
+    public java.sql.Connection getConnection() {
+        return connection;
+    }
+
+    
+}
